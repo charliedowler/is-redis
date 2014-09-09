3e3272898542acc18d2e414403fe9ff266195707
charliedowler <charliedowler15@gmail.com>
2014-09-09 22:56:19 +0100

    fixed typo

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
 'use strict';
 var assert = require('assert');
 var exec = require('child_process').exec;
-var isMysql = require('../index');
+var isRedis = require('../index');
 
 it('Should recognise redis is running - promise', function(done) {
-  isMysql().then(function(db) {
+  isRedis().then(function(db) {
     assert.equal(db.running, true);
     assert.equal(db.type, 'redis');
     done();
   });
 });
 it('Should recognise redis is running - callback', function(done) {
-  isMysql(function(db) {
+  isRedis(function(db) {
     assert.equal(db.running, true);
     assert.equal(db.type, 'redis');
     done();
